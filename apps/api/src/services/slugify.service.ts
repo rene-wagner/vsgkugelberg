11a4c78e828a2d68c255ed11ae0be95facfb6592
René Wagner <rene.papenfuss@getaway.group>
2025-11-18 16:21:00 +0100

    updated the slugify service

--- apps/api/src/services/slugify.service.ts
+++ apps/api/src/services/slugify.service.ts
@@ -2,6 +2,10 @@ import slugify from 'slugify'
 
 export class SlugifyService {
   slugify(text: string): string {
-    return slugify(text, { lower: true, strict: true })
+    return slugify(text, {
+      lower: true,
+      strict: true,
+      locale: 'de',
+    })
   }
 }
