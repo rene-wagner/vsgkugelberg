f90dc6202aed2c2dd03fc666f3a788a5d6311fb6
René Wagner <rene.papenfuss@getaway.group>
2025-12-07 17:03:03 +0100

    added block component for headings

---
+++ apps/web/src/components/blocks/VsgBlockHeadline.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+import { computed } from 'vue';
+
+const props = withDefaults(
+  defineProps<{
+    level?: 1 | 2 | 3 | 4 | 5 | 6;
+    content: string;
+  }>(),
+  {
+    level: 1,
+  }
+);
+
+const tag = computed(() => `h${props.level}`);
+</script>
+
+<template>
+  <component :is="tag">{{ content }}</component>
+</template>
